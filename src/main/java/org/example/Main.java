97d850107d4bb3adb7fe79d739249d73d0f6af98
Andrés Felipe Alarcón Penagos <afalarco@unal.edu.co>
2022-11-26 09:01:18 -0500

    Se agrega plantilla del menú principal

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,52 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner read = new Scanner(System.in);
+        System.out.println("********* Aplicación de viáticos *********");
+        int option;
+
+        do{
+            System.out.println("1. Registrar empleado \n" +
+                    "2. Registrar viajes (trasnporte, hospedaje, alimentación) \n" +
+                    "3. Ver empleados \n" +
+                    "4. Ver viajes de un empleado\n" +
+                    "0. Salir ");
+            option = read.nextInt();
+
+            switch (option){
+                case 0:
+                    break;
+
+                case 1:
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                default:
+                    System.out.println("Opcion no valida");
+            }
+
+
+
+        }while(option != 0);
+
+
+
+
+
+
+
+
     }
 }
